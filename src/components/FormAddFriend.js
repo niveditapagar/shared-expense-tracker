e2e0f93619eabd99ae266a627bc3c970af2a4b56
Nivedita <nivedita.pagar@outlook.com>
2023-12-01 11:59:35 +0100

    Implement controlled elements for friend details; Add handle submit functionality for adding new friend; Clear inputs on submission

--- src/components/FormAddFriend.js
+++ src/components/FormAddFriend.js
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import Button from "./Button";
 
 export default function FormAddFriend() {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+    const id = crypto.randomUUID();
+    const newFriend = { id: id, name, image: `${image}?=${id}`, balance: 0 };
+    console.log(newFriend);
+    setName("");
+    setImage("https://i.pravatar.cc/48");
+  }
+
   return (
-    <form className="form-add-friend">
+    <form className="form-add-friend" onSubmit={handleSubmit}>
       <label>👯‍♀️ Friend name</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label>🔗 Image URL</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={image}
+        onChange={(e) => setImage(e.target.value)}
+      />
       <Button>Add</Button>
     </form>
   );
